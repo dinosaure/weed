6f705af8cd819f6516c4ff53a9e4cadf08d97131
Romain Calascibetta <romain.calascibetta@gmail.com>
2025-05-18 17:20:26 +0200

    Continue

--- unikernel.ml
+++ unikernel.ml
@@ -18,10 +18,18 @@ let load_func_from_module ls mod_id f_name =
   | None -> Error (`Unbound_name f_name)
   | Some v -> Ok (v, env_id)
 
-let run_modul () =
+let block_device_to_string size blk =
+  let pagesize = Miou_solo5.Block.pagesize blk in
+  let size_aligned = (size + pagesize - 1) / pagesize * pagesize in
+  let bstr = Bstr.create size_aligned in
+  for i = 0 to (size_aligned / pagesize) - 1 do
+    Miou_solo5.Block.atomic_read blk ~off:(i * pagesize) ~dst_off:(i * pagesize) bstr
+  done;
+  Bstr.sub_string bstr ~off:0 ~len:size
 
-  let* m = Owi.Parse.Binary.Module.from_file (Fpath.v "./patched.wasm") in
-
+let run_modul ~size blk =
+  let str = block_device_to_string size blk in
+  let* m = Owi.Parse.Binary.Module.from_string str in
   let link_state =
     Link.extern_module Link.empty_state ~name:"env"
       Concrete_wasm_ffi.env_extern_module
@@ -30,23 +38,65 @@ let run_modul () =
     Link.extern_module link_state ~name:"wasi_snapshot_preview1"
       Concrete_wasm_ffi.wasi_snapshot_preview1_extern_module
   in
-
   let* m, link_state = Link.modul link_state ~name:None m in
-
   let* () = Interpret.Concrete.modul link_state.envs m in
-
   let* f, env_id = load_func_from_module link_state None "sqlite3_open" in
-
   Interpret.Concrete.exec_vfunc_from_outside ~locals:[ V.I32 0l; I32 0l ] ~env:env_id
     ~envs:link_state.envs f
 
-let () =
-  Logs.set_level (Some Logs.Info);
-  Logs.set_reporter (Logs_fmt.reporter ());
-  let result = run_modul () in
+let run _ size = Miou_solo5.(run [ block "wasm" ]) @@ fun blk () ->
+  let result = run_modul ~size blk in
   match result with
   | Error e ->
     let s = Owi.Result.err_to_string e in
     Fmt.pr "error: %s@\n" s;
     assert false
   | Ok _ -> ()
+
+open Cmdliner
+
+let output_options = "OUTPUT OPTIONS"
+
+let verbosity = Logs_cli.level ~docs:output_options ()
+let renderer = Fmt_cli.style_renderer ~docs:output_options ()
+
+let utf_8 =
+  let doc = "Allow binaries to emit UTF-8 characters." in
+  Arg.(value & opt bool true & info [ "with-utf-8" ] ~doc)
+
+let t0 = Miou_solo5.clock_monotonic ()
+
+let reporter ppf =
+  let report src level ~over k msgf =
+    let k _ =
+      over () ;
+      k () in
+    let with_metadata header _tags k ppf fmt =
+      let t1 = Miou_solo5.clock_monotonic () in
+      let delta = Float.of_int (t1 - t0) in
+      let delta = delta /. 1_000_000_000. in
+      Fmt.kpf k ppf
+        ("[+%a][%a]%a[%a]: " ^^ fmt ^^ "\n%!")
+        Fmt.(styled `Blue (fmt "%04.04f")) delta
+        Fmt.(styled `Cyan int)
+        (Stdlib.Domain.self () :> int)
+        Logs_fmt.pp_header (level, header)
+        Fmt.(styled `Magenta string)
+        (Logs.Src.name src) in
+    msgf @@ fun ?header ?tags fmt -> with_metadata header tags k ppf fmt in
+  { Logs.report }
+
+let setup_logs utf_8 style_renderer level =
+  Option.iter (Fmt.set_style_renderer Fmt.stdout) style_renderer;
+  Fmt.set_utf_8 Fmt.stdout utf_8;
+  Logs.set_level level ;
+  Logs.set_reporter (reporter Fmt.stdout) ;
+  Option.is_none level
+
+let setup_logs = Term.(const setup_logs $ utf_8 $ renderer $ verbosity)
+
+let size = Arg.(required & opt (some int) None & info [ "s"; "size"])
+let term = Term.(const run $ setup_logs $ size)
+let info = Cmd.info "unikernel"
+let cmd = Cmd.v info term
+let () = Cmd.(exit @@ eval cmd)
